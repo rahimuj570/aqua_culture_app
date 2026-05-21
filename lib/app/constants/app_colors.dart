9b742331c9f53b1e9a42aa41061862f3e6d72293
rahimuj570 <rahimuj570@gmail.com>
2026-05-21 16:44:12 +0600

    feat: color constans are added

---
+++ lib/app/constants/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color appThemeColor = Color(0xff0E4D6E);
+  static const Color secondaryThemeColor = Color(0xff1A8A6E);
+  static const Color scafoldColor = Color(0xffF1F6FF);
+
+  static const Color green = Color(0xff28B67A);
+  static const Color red = Color(0xffE53E3E);
+  static const Color redLight = Color(0xffFED7D7);
+
+  static const Color orange = Color(0xffF0AE40);
+  static const Color orangeLight = Color(0xffFEEBCB);
+
+  static const Color infoLight = Color(0xffEBF8FF);
+  static const Color infoText = Color(0xff0076A9);
+
+  static const Color cancelButtonBackground = Color(0xffE7EDF1);
+  static const Color greyText = Color(0xff475569);
+}
